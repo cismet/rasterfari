0b156e1f90a6e8fea074276dd97df4642fad8f7a
helllth <th@cismet.de>
2016-01-08 14:43:40 +0100

    added example docs, example config and example layer2doc converter

---
+++ custom.js
@@ -0,0 +1,10 @@
+
+
+
+exports.customConvertLayerPartToDoc = function getDocPathFromLayerPart(layerPart) {
+    if (layerPart.startsWith('R')) {
+        return  "./docs/bplaene_etrs/rechtsk/B" + layerPart.substring(1, layerPart.length) + ".tif";
+    } else if (layerPart.startsWith('N')) {
+        return  "./docs/bplaene_etrs/nicht_rechtsk/B" + layerPart.substring(1, layerPart.length) + ".tif";
+    }
+};
